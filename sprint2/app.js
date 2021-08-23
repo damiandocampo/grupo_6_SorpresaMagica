2d8c29b99b36bab185077cd558cc648e4b7a0061
soledadrv <soledadrv@outlook.com>
2021-08-23 17:51:15 -0300

    agrego rutas e imágenes

--- sprint2/app.js
+++ sprint2/app.js
@@ -4,13 +4,29 @@ const path= require('path')
 
 app.use(express.static('public'))
 
+
 /*rutas */
+
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, 'views' ,'home.html' ))
 })
+
 app.get('/carrito',(req,res) => {
     res.sendFile(path.join(__dirname, 'views' ,'carritoDeCompras.html' ))
 })
+
+app.get('/login',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views' ,'login.html' ))
+})
+
+app.get('/registro',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views' ,'registro.html' ))
+})
+
+app.get('/detalle',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views' ,'detalleDeProductos.html' ))
+})
+
 
 const port= 3030
 
