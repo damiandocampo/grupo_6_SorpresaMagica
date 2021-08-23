6651c039383aacf2c3bc942b01857eb8efa8097b
damiandocampo <contactosmile@yahoo.com>
2021-08-23 15:33:42 -0300

    agrego ruta carrito y los import del css

--- sprint2/app.js
+++ sprint2/app.js
@@ -3,8 +3,13 @@ const app= express()
 const path= require('path')
 
 app.use(express.static('public'))
+
+/*rutas */
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, 'views' ,'home.html' ))
+})
+app.get('/carrito',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views' ,'carritoDeCompras.html' ))
 })
 
 const port= 3030
