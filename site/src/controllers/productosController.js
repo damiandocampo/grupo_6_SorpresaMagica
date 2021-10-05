6e1282a445685be1cebed783cf911bb28ce9f278
ClaudiaCZ <claudiavcasco@gmail.comgit config --global user.email git config --global user.email claudiavcasco@gmail.comgit config --global user.email claudiavcasco@gmail.com>
2021-10-05 15:00:26 -0300

    detalle dinamico GRUD terminado

--- site/src/controllers/productosController.js
+++ site/src/controllers/productosController.js
@@ -1,6 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productos.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const controller = {
     detalle: (req,res) => {
-        res.render('detalleDeProductos');
+        const {id} = req.params
+        const product = products.find(element => element.id === +id)
+
+        res.render('detalleDeProductos', {product, products});
     },
 }
 
--- site/src/routes/productos.js
+++ site/src/routes/productos.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {detalle} = require('../controllers/productosController')
 
-router.get('/detalle', detalle);
+router.get('/detalle/:id', detalle);
 
 module.exports = router;
