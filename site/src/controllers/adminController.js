269467c573ee4aaae82af2b8f9bde8ce989b683d
ClaudiaCZ <claudiavcasco@gmail.comgit config --global user.email git config --global user.email claudiavcasco@gmail.comgit config --global user.email claudiavcasco@gmail.com>
2021-10-07 00:18:55 -0300

    edicíon de producto

--- site/src/controllers/adminController.js
+++ site/src/controllers/adminController.js
@@ -1,4 +1,9 @@
-const products = require('../data/productos.json');
+const fs = require('fs');
+const path = require('path');
+const router = require('../routes/admin');
+
+const productsFilePath = path.join(__dirname, '../data/productos.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const controller = {
     list: function(req, res, next) {
@@ -8,8 +13,26 @@ const controller = {
         res.render('./admin/create');
     },
     edit: function(req, res, next) {
-        res.render('./admin/edit');
+        const product = products.find(e => e.id === +req.params.id)
+        res.render('./admin/edit', {product});
     },
+    update: (req, res) =>{
+        const productUpdate = products.find(product => product.id === +req.params.id)
+        const { marca, title, price, categoria, descuento,} = req.body
+        if(productUpdate) {
+            productUpdate.marca = marca
+            productUpdate.title = title
+            productUpdate.price = +price
+            productUpdate.categoria = categoria
+            productUpdate.descuento = +descuento
+
+            fs.writeFileSync(productsFilePath, JSON.stringify(products))
+
+            res.redirect(`admin/admin/${req.params.id}`)
+        }else{
+            res.redirect('/')
+        }
+    }
 };
 
 module.exports = controller;
--- site/src/routes/admin.js
+++ site/src/routes/admin.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {list, create, edit} = require('../controllers/adminController')
+const {list, create, edit, update} = require('../controllers/adminController')
 
 
 router.get('/', list);
 router.get('/create', create);
-router.get('/edit', edit);
 
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id', edit);
+router.put('/edit/:id', update)
 module.exports = router;
