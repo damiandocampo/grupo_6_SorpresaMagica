bf52650a3cc58335b96fa175857c3ea5fd930b87
aguustina06 <agustina_0610@hotmail.com>
2021-10-07 13:51:27 -0300

    registro

--- site/src/routes/productos.js
+++ site/src/routes/productos.js
@@ -4,4 +4,31 @@ const {detalle} = require('../controllers/productosController')
 
 router.get('/detalle', detalle);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*router.delete('/delete/:id', productosBorrados);*/
+
+
+
 module.exports = router;
--- site/src/routes/users.js
+++ site/src/routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { login, registro} = require('../controllers/userController')
+const { login, registro, registre} = require('../controllers/userController')
 
 /* GET users listing. */
 
 router.get('/login', login);
 
 router.get('/registro', registro);
-
+router.post('/registro', registre)
 module.exports = router;
