b56b497b710c1b66f36c9fa6cc6d68b9cdb6332c
aguustina06 <agustina_0610@hotmail.com>
2021-11-11 14:25:50 -0300

    migrations,models

---
+++ site/src/database/migrations/20211111153704-create-user.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+
+      rolId: {
+        type: Sequelize.INTEGER,
+
+        references: {
+          model: {
+            tableName: 'Carts'
+          },
+          key: 'id'
+        }
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
