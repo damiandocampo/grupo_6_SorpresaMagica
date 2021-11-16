d555e0e687e5b53cd8e96f31c804cfcecdfc23ce
aguustina06 <agustina_0610@hotmail.com>
2021-11-16 16:09:38 -0300

    seeders

---
+++ site/src/database/seeders/20211116173451-create-users.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('users', [
+      {
+        first_name: nombre.trim(),
+        last_name: apellido.trim(),
+        email: email.trim(),
+        password: bcrypt.hashSync(contraseña, 10),
+        image: 'default-image.png',
+        rolId: 1,
+        createdAt: new Date,
+        updateAt: new Date
+    }
+  ],  {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.bulkDelete('users', null, {});
+     
+  }
+};
+
+
---
+++ site/src/database/seeders/20211116174113-create-rol.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('rols', 
+    [
+      {
+      name: 'admin',
+      createdAt : new Date,
+      updatedAt : new Date
+      },
+      {
+      name: 'user',
+      createdAt : new Date,
+      updatedAt : new Date
+      }
+    ], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.bulkDelete('rols', null, {});
+     
+  }
+};
